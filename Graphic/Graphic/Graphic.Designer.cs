a3847adc6c4cfbd8b0b5ab7237eefdd9bd640e8c
UIT19522006 <67238266+UIT19522006@users.noreply.github.com>
2020-12-24 20:54:20 +0700

    Game Sprite

---
+++ Graphic/Graphic/Graphic.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Graphic
+{
+	partial class Graphic
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cbbAction = new System.Windows.Forms.ComboBox();
+			this.btPause = new System.Windows.Forms.Button();
+			this.cbbAction2 = new System.Windows.Forms.ComboBox();
+			this.btPause2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// cbbAction
+			// 
+			this.cbbAction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbbAction.Font = new System.Drawing.Font("SVN-Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.cbbAction.FormattingEnabled = true;
+			this.cbbAction.Location = new System.Drawing.Point(162, 12);
+			this.cbbAction.Name = "cbbAction";
+			this.cbbAction.Size = new System.Drawing.Size(173, 35);
+			this.cbbAction.TabIndex = 0;
+			// 
+			// btPause
+			// 
+			this.btPause.Font = new System.Drawing.Font("SVN-Agency FB", 15.75F);
+			this.btPause.Location = new System.Drawing.Point(354, 13);
+			this.btPause.Name = "btPause";
+			this.btPause.Size = new System.Drawing.Size(83, 34);
+			this.btPause.TabIndex = 1;
+			this.btPause.Text = "Pause";
+			this.btPause.UseVisualStyleBackColor = true;
+			this.btPause.Click += new System.EventHandler(this.btPause_Click);
+			// 
+			// cbbAction2
+			// 
+			this.cbbAction2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbbAction2.Font = new System.Drawing.Font("SVN-Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.cbbAction2.FormattingEnabled = true;
+			this.cbbAction2.Location = new System.Drawing.Point(162, 138);
+			this.cbbAction2.Name = "cbbAction2";
+			this.cbbAction2.Size = new System.Drawing.Size(173, 35);
+			this.cbbAction2.TabIndex = 0;
+			// 
+			// btPause2
+			// 
+			this.btPause2.Font = new System.Drawing.Font("SVN-Agency FB", 15.75F);
+			this.btPause2.Location = new System.Drawing.Point(354, 137);
+			this.btPause2.Name = "btPause2";
+			this.btPause2.Size = new System.Drawing.Size(83, 36);
+			this.btPause2.TabIndex = 1;
+			this.btPause2.Text = "Pause";
+			this.btPause2.UseVisualStyleBackColor = true;
+			this.btPause2.Click += new System.EventHandler(this.btPause2_Click);
+			// 
+			// Graphic
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(502, 395);
+			this.Controls.Add(this.btPause2);
+			this.Controls.Add(this.btPause);
+			this.Controls.Add(this.cbbAction2);
+			this.Controls.Add(this.cbbAction);
+			this.Name = "Graphic";
+			this.Text = "Graphic";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox cbbAction;
+		private System.Windows.Forms.Button btPause;
+		private System.Windows.Forms.ComboBox cbbAction2;
+		private System.Windows.Forms.Button btPause2;
+	}
+}
---
+++ Graphic/Graphic/Graphic.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Graphic
+{
+	public partial class Graphic : Form
+	{
+
+		private Bitmap sprite, sprite2;
+		private Bitmap backBuffer, backBuffer2;
+		private Timer timer, timer2;
+		public Graphics graphics, graphics2;
+		private int index, index2;
+		int limit = 5, limit2 = 3;
+		List<string> Actions = new List<string>(), Actions2 = new List<string>();
+		public Graphic()
+		{
+			InitializeComponent();
+			
+			graphics = this.CreateGraphics();
+			graphics2 = this.CreateGraphics();
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			backBuffer = new Bitmap(150,150);
+			backBuffer2 = new Bitmap(150, 150);
+			sprite = new Bitmap("NewSprite.png");
+			sprite2 = new Bitmap("Boss.png");
+			index = 0;
+			index2 = 0;
+			timer = new Timer();
+			timer.Enabled = true;
+			timer.Interval = 100;
+			timer.Tick += new EventHandler(timer_Tick);
+			Actions = new List<string>{ "Idle","Walk","Attack","Dizzy","Die","Defend"};
+			this.cbbAction.DataSource = Actions;
+			cbbAction.SelectedIndex = 0;
+			Actions2 = new List<string> { "Jump", "Attack", "Hurt","Die","Walk","Run","Sneer","Attack" };
+			this.cbbAction2.DataSource = Actions2;
+			cbbAction2.SelectedIndex = 0;
+			timer2 = new Timer();
+			timer2.Tick += new EventHandler(timer2_Tick);
+			timer2.Enabled = true;
+			timer2.Interval = 100;
+		}
+		private void Render()
+		{
+			Graphics g = Graphics.FromImage(backBuffer);
+			g.Clear(Color.White);
+			MultiChoice(g);
+			g.Dispose();
+			index++;
+			if (index >= limit)
+			{
+				index = 0;
+				//int k = cbbAction.SelectedIndex + 1;
+				//if (k >= Actions.Count)
+				//{
+				//	cbbAction.SelectedIndex = 0;
+				//}
+				//else
+				//	cbbAction.SelectedIndex = k;
+			}
+			else
+			{
+				index++;
+			}
+
+		}
+		public void MultiChoice(Graphics g)
+		{
+			if(cbbAction.SelectedIndex==0)
+			{
+				g.DrawImage(sprite, 0, 0,
+					new Rectangle(index * 68, 0, 68, 92), GraphicsUnit.Pixel);
+				limit = 5;
+				timer.Interval = 100;
+			}	
+			else if(cbbAction.SelectedIndex == 1)
+			{
+				g.DrawImage(sprite, 0, 0,
+					new Rectangle(index * 71 , 94, 71, 92), GraphicsUnit.Pixel);
+				limit = 5;
+				timer.Interval = 100;
+			}
+			else if (cbbAction.SelectedIndex == 2)
+			{
+				g.DrawImage(sprite, 0, 0,
+					new Rectangle(index * 135, 192, 135, 97), GraphicsUnit.Pixel);
+				timer.Interval = 100;
+				limit = 5;
+			}
+			else if (cbbAction.SelectedIndex == 3)
+			{
+				g.DrawImage(sprite, 0, 0,
+					new Rectangle(index * 86, 300, 86, 86), GraphicsUnit.Pixel);
+				timer.Interval = 100;
+				limit = 2;
+			}
+			else if (cbbAction.SelectedIndex == 4)
+			{
+				g.DrawImage(sprite, 0, 0,
+					new Rectangle(index * 100, 400, 96, 100), GraphicsUnit.Pixel);
+				timer.Interval = 150;
+				limit = 8;
+			}
+			else if(cbbAction.SelectedIndex==5)
+			{
+				g.DrawImage(sprite, 0, 0,
+					new Rectangle(index * 150, 550, 130, 90), GraphicsUnit.Pixel);
+				timer.Interval = 200;
+				limit = 4;
+			}	
+		}
+		public void MultiChoice2(Graphics g)
+		{
+			if (cbbAction2.SelectedIndex == 0)
+			{
+				g.DrawImage(sprite2, 0, 0,
+					new Rectangle(index2 * 96, 0, 96, 96), GraphicsUnit.Pixel);
+				limit2 = 3;
+				timer2.Interval = 200;
+			}
+			else if (cbbAction2.SelectedIndex == 1)
+			{
+				g.DrawImage(sprite2, 0, 0,
+					new Rectangle(index2 * 96, 103, 96, 96), GraphicsUnit.Pixel);
+				limit2 = 3;
+				timer2.Interval = 200;
+			}
+			else if (cbbAction2.SelectedIndex == 2)
+			{
+				g.DrawImage(sprite2, 0, 0,
+					new Rectangle(index2 * 96, 206, 96, 96), GraphicsUnit.Pixel);
+				limit2 = 3;
+				timer2.Interval = 200;
+			}
+			else if (cbbAction2.SelectedIndex == 3)
+			{
+				g.DrawImage(sprite2, 0, 0,
+					new Rectangle(index2 * 96, 308, 96, 96), GraphicsUnit.Pixel);
+				limit2 = 5;
+				timer2.Interval = 200;
+			}
+			else if (cbbAction2.SelectedIndex == 4)
+			{
+				g.DrawImage(sprite2, 0, 0,
+					new Rectangle(index2 * 96, 404, 96, 96), GraphicsUnit.Pixel);
+				limit2 = 5;
+				timer2.Interval = 200;
+			}
+			else if (cbbAction2.SelectedIndex == 5)
+			{
+				g.DrawImage(sprite2, 0, 0,
+					new Rectangle(index2 * 96, 500, 96, 96), GraphicsUnit.Pixel);
+				limit2 = 5;
+				timer2.Interval = 200;
+			}
+			else if (cbbAction2.SelectedIndex == 6)
+			{
+				g.DrawImage(sprite2, 0, 0,
+					new Rectangle(index2 * 96, 597, 96, 96), GraphicsUnit.Pixel);
+				limit2 = 5;
+				timer2.Interval = 200;
+			}
+			else if (cbbAction2.SelectedIndex == 7)
+			{
+				g.DrawImage(sprite2, 0, 0,
+					new Rectangle(index2 * 96, 690, 96, 96), GraphicsUnit.Pixel);
+				limit2 = 5;
+				timer2.Interval = 200;
+			}
+		}
+		private void Render2()
+		{
+			Graphics g = Graphics.FromImage(backBuffer2);
+			g.Clear(Color.White);
+			MultiChoice2(g);
+			g.Dispose();
+			index2++;
+			if (index2 >= limit2)
+			{
+				index2 = 0;
+				//int k = cbbAction.SelectedIndex + 1;
+				//if (k >= Actions.Count)
+				//{
+				//	cbbAction.SelectedIndex = 0;
+				//}
+				//else
+				//	cbbAction.SelectedIndex = k;
+			}
+			else
+			{
+				index++;
+			}
+
+		}
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			Render();
+			// Vẽ lên màn hình
+			graphics.DrawImageUnscaled(backBuffer, 0, 0);
+		}
+		private void btPause_Click(object sender, EventArgs e)
+		{
+			if(btPause.Text=="Pause")
+			{
+				timer.Enabled = false;
+				btPause.Text = "Play";
+			}
+			else
+			{
+				timer.Enabled = true;
+				btPause.Text = "Pause";
+			}	
+		}
+		private void btPause2_Click(object sender, EventArgs e)
+		{
+			if (btPause2.Text == "Pause")
+			{
+				timer2.Enabled = false;
+				btPause2.Text = "Play";
+			}
+			else
+			{
+				timer2.Enabled = true;
+				btPause2.Text = "Pause";
+			}
+		}
+		private void timer2_Tick(object sender, EventArgs e)
+		{
+			Render2();
+			// Vẽ lên màn hình
+			graphics2.DrawImageUnscaled(backBuffer2, 200, 200);
+		}
+	}
+}
